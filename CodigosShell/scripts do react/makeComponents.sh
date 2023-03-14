be4a810f3a0faed35f23f174a76d626e435f00ee
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2023-03-14 00:37:22 -0300

    scripts do react - inserido conteúdo nos arquivos de estilização

--- CodigosShell/scripts do react/makeComponents.sh
+++ CodigosShell/scripts do react/makeComponents.sh
@@ -15,9 +15,17 @@ read -p "Informe o nome do componente: " compName
 #Pesquisa se a pasta existe, salva o caminho entra no diretório e cria a pasta do componente.
 folder=`find */$locationFolder -maxdepth 0`
 
+#Pesquisa se existe um componente com o nome digitado.
+componentExist=`find $folder/$compName -maxdepth 0`
+
 if [ $folder ]; then
-  cd $folder
-  mkdir $compName 
+  if [ $componentExist ]; then
+    echo -e "$colorRED Esse componente já existe! $colorRESET"
+    exit
+  else
+    cd $folder
+    mkdir $compName 
+  fi
 else
   echo -e "$colorRED O diretório especificado não existe! $colorRESET"
   exit
@@ -28,16 +36,49 @@ fi
 read -p "Qual o tipo do arquivo styles CSS[c], SCSS[s], TS[t]: " op
 cd $compName
 
+#Conteúdo do arquivo styles CSS e SCSS.
+styleContentCSS=`echo -e "@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+  
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+  font-family: 'Lato', sans-serif;
+}"`   
+
+#Conteúdo do arquivo styles TS.
+styleContentTS=`echo -e "import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});"`
+
+
 case $op in
   c)
     > styles.css;
-    stylesImport="'./styles.css'";;
+    stylesImport="'./styles.css'"
+    echo "$styleContentCSS" >> styles.css
+    reactNativeImport=""
+    reactNativeView="";;
   s)
     > styles.scss;
-    stylesImport="'./styles.scss'";;
+    stylesImport="'./styles.scss'"
+    echo "$styleContentCSS" >> styles.scss
+    reactNativeImport=""
+    reactNativeView="";;
   t)
     > styles.ts;
-    stylesImport="{ styles } from './styles'";;
+    stylesImport="{ styles } from './styles'"
+    echo "$styleContentTS" >> styles.ts
+    reactNativeImport="import { View } from 'react-native';"
+    reactNativeView="<View>Hello ReactNative</View>";;
   *)
     cd ..
     rm -dr $compName
@@ -49,13 +90,14 @@ esac
 # Cria o arquivo index.tsx e preenche o mesmo com o script básico.
 > index.tsx
 
-echo -e "import React from 'react'; \n
-import $stylesImport; \n 
+echo -e "import React from 'react'; 
+$reactNativeImport\n 
+import $stylesImport;  
 \n
 export function $compName() { \n
-\t return ( \n
-
-\t ); \n
+  return ( 
+    $reactNativeView
+  ); 
 }" >> index.tsx
 
 
